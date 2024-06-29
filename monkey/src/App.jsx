00d6d2d5336f5b9eb476fe07be6728825af5c708
SAEED NM <saeedmoozhikkal@gmail.com>
2024-06-29 16:02:09 +0530

    updation

--- monkey/src/App.jsx
+++ monkey/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './component/HomePage'
 import Navbar from './component/Navbar/Navbar'
 import SecondPage from './component/secondPage'
 import Thirdframe from './component/Frame5/frame3'
+import Sixthframe from './component/Frame6/frame6'
 function App() {
   
 
@@ -12,6 +13,7 @@ function App() {
     <HomePage/>
     <SecondPage/>
     <Thirdframe/>
+    <Sixthframe/>
    </div>
   )
 }
--- monkey/src/component/Frame6/frame6.jsx
+++ monkey/src/component/Frame6/frame6.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import { Carousel } from "flowbite-react";
 const Sixthframe = () =>{
     return(
-        <div>
-            <h1>hkisgh</h1>
-        </div>       
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel slide={false}>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>        
     )
 }
 export default Sixthframe;
